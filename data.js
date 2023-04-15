8ca7525b3e16eb1ad1acc3d5addf7cafde07a369
Amber <amberkramsey@gmail.com>
2023-04-14 20:16:13 -0500

    initial commit

---
+++ data.js
@@ -0,0 +1,90 @@
+window.data = {
+  book: 0,
+  totalCPS: 0,
+  producers: [
+    {
+      id: 'bookmark_set',
+      price: 10,
+      unlocked: false,
+      cps: 1,
+      qty: 0
+    },
+    {
+      id: 'book_bag',
+      price: 50,
+      unlocked: false,
+      cps: 2,
+      qty: 0
+    },
+    {
+      id: 'book_cover',
+      price: 100,
+      unlocked: false,
+      cps: 5,
+      qty: 0
+    },
+    {
+      id: 'small_book_shelf',
+      price: 500,
+      unlocked: false,
+      cps: 10,
+      qty: 0
+    },
+    {
+      id: 'large_book_shelf',
+      price: 1000,
+      unlocked: false,
+      cps: 20,
+      qty: 0
+    },
+    {
+      id: 'e_reader',
+      price: 5000,
+      unlocked: false,
+      cps: 50,
+      qty: 0
+    },
+    {
+      id: 'ten_free_books',
+      price: 10000,
+      unlocked: false,
+      cps: 75,
+      qty: 0
+    },
+    {
+      id: 'twenty_free_books',
+      price: 50000,
+      unlocked: false,
+      cps: 100,
+      qty: 0
+    },
+    {
+      id: 'bookstore',
+      price: 100000,
+      unlocked: false,
+      cps: 200,
+      qty: 0
+    },
+    {
+      id: 'dream_library',
+      price: 500000,
+      unlocked: false,
+      cps: 500,
+      qty: 0
+    },
+    {
+      id: 'meet_favorite_author',
+      price: 1000000,
+      unlocked: false,
+      cps: 1000,
+      qty: 0
+    },
+    {
+      id: 'unlimited_books',
+      price: 5000000,
+      unlocked: false,
+      cps: 2000,
+      qty: 0
+    }
+  ]
+};
